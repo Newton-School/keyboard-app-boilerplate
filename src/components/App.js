c3b079f8bd3c0fa3068e95034632173593e92799
Arnab-31 <57807670+Arnab-31@users.noreply.github.com>
2023-04-11 17:06:41 +0530

    Solve

--- src/components/App.js
+++ src/components/App.js
@@ -1,56 +1,27 @@
-import React, { useState } from "react";
+import "../styles/App.css";
+import React, { useState, useEffect } from "react";
+
+const keys = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
 
 const App = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-  const [loggedIn, setLoggedIn] = useState(false);
+  const [input, setInput] = useState("");
+  const [quote, setQuote] = useState(null);
 
-  const handleSubmit = async event => {
-    event.preventDefault();
-    try {
-      const response = await fetch("https://reqres.in/api/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ email, password })
-      });
-      const data = await response.json();
-      if (data.error) {
-        setError(data.error);
-      } else {
-        setLoggedIn(true);
-      }
-    } catch (err) {
-      setError("An error occurred. Please try again later.");
-    }
-  };
+  
+  if (quote) {
+    return <div className="quote">{quote}</div>;
+  }
 
   return (
-    <div>
-      {loggedIn ? (
-        <h1>Welcome User!</h1>
-      ) : (
-        <form onSubmit={handleSubmit}>
-          <input
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-            required
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            required
-          />
-          <button type="submit">Submit</button>
-          {error && <p style={{ color: "red" }}>{error}</p>}
-        </form>
-      )}
+    <div className="keyboard">
+      <div className="preview">{input}</div>
+      <div>
+        {keys.map((key) => (
+          <button key={key} id={key === " " ? `key-space` : `key-${key}`}>
+            {key === " " ? "Space" : key.toUpperCase()}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
